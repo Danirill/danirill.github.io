0aa479ae976172b4884d80ca526e580bd0d4cd01
Danirill <dzykin3@gmail.com>
2020-01-21 21:31:48 +0500

    Updates

---
+++ precache-manifest.f510113a3b140e562f1459ac07fc6433.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1dbd3a0d4b8be8ba9cb2f6e4f6649e7",
+    "url": "/leader/index.html"
+  },
+  {
+    "revision": "e5ba96cedc1d0cdba5bf",
+    "url": "/leader/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "b2feb321666f081e60a8",
+    "url": "/leader/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "e5ba96cedc1d0cdba5bf",
+    "url": "/leader/static/js/2.64d99af7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/leader/static/js/2.64d99af7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2feb321666f081e60a8",
+    "url": "/leader/static/js/main.8f549699.chunk.js"
+  },
+  {
+    "revision": "c21edded07a224a35919",
+    "url": "/leader/static/js/runtime-main.2d9e664a.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/leader/static/media/persik.4e1ec840.png"
+  }
+]);
